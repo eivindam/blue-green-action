e95723e6864e02954ef51bee5c978d0de27a9da3
Eivind Moland <eivind.moland@phonero.no>
2021-03-09 11:25:57 +0100

    aws credentials test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ KUBE_CONFIG=$1
 DEPLOY_PATH=$2
 
 mkdir ~/.kube/
+mkdir ~/.aws/
+
+printf "[profile test_deploy]\nregion=%s\n\naws_access_key_id=%s\naws_secret_access_key=%s\n" $AWS_REGION $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY > ~/.aws/credentials
+
 echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 
 kubectl apply -f $DEPLOY_PATH
