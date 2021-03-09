f9102087a1e60dc6c0c1596a7b3e1f88a8167e15
Eivind Moland <eivind.moland@phonero.no>
2021-03-09 12:56:52 +0100

    bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ KUBE_CONFIG=$1
 DEPLOY_PATH=$2
 BRANCH_NAME=$(echo $GITHUB_REF | cut -d'/' -f 3)
 GITHUB_SHA_SHORT=$(echo $GITHUB_SHA | cut -c1-7)
-DEPLOY_VERSION = ${GITHUB_SHA_SHORT}
+VERSION = ${GITHUB_SHA_SHORT}
 NAMESPACE=default
 DEPLOY_NAME=websocket
 
@@ -25,16 +25,16 @@ echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 # Deploy
 CURRENT_VERSION = $(kubectl get service $DEPLOY_NAME -o=jsonpath='{.spec.selector.version}' --namespace=${NAMESPACE})
 
-if [ "$CURRENT_VERSION" == "$DEPLOY_VERSION" ]; then
-   echo "[DEPLOY] Both versions are the same: $DEPLOY_VERSION"
+if [ "$CURRENT_VERSION" == "$VERSION" ]; then
+   echo "[DEPLOY] Both versions are the same: $VERSION"
    exit 0
 fi
 
-kubectl get deployment $DEPLOY_NAME-$CURRENT_VERSION -o=yaml --namespace=${NAMESPACE} | sed -e "s/$CURRENT_VERSION/$DEPLOY_VERSION/g" | kubectl apply --namespace=${NAMESPACE} -f -
-kubectl rollout status deployment/$DEPLOY_NAME-$DEPLOY_VERSION --namespace=${NAMESPACE}
+kubectl get deployment $DEPLOY_NAME-$CURRENT_VERSION -o=yaml --namespace=${NAMESPACE} | sed -e "s/$CURRENT_VERSION/$VERSION/g" | kubectl apply --namespace=${NAMESPACE} -f -
+kubectl rollout status deployment/$DEPLOY_NAME-$VERSION --namespace=${NAMESPACE}
 
 sleep 10
 
-echo "[DEPLOY] $(kubectl get pods -l version="$DEPLOY_VERSION" -n $NAMESPACE --no-headers)"
+echo "[DEPLOY] $(kubectl get pods -l version="$VERSION" -n $NAMESPACE --no-headers)"
 
 #activate kubectl get service $SERVICE_NAME -o=yaml --namespace=${NAMESPACE} | sed -e "s/$CURRENT_VERSION/$DEPLOY_VERSION/g" | kubectl apply --namespace=${NAMESPACE} -f - 
