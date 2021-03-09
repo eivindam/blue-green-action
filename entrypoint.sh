2570b02ad47bb34129269f3416b104955e3896bf
Eivind Moland <eivind.moland@phonero.no>
2021-03-09 11:15:58 +0100

    syntax issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ KUBE_CONFIG=$1
 DEPLOY_PATH=$2
 
 mkdir ~/.kube/
-echo $KUBE_CONFIG | base64 --decode > ~/.kube/config
+echo $KUBE_CONFIG | base64 -D > ~/.kube/config
 
 kubectl apply -f $DEPLOY_PATH
