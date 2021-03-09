e266e7b10a14a8154d1cb9f13fe42f5a575c4a55
Eivind Moland <eivind.moland@phonero.no>
2021-03-09 11:49:40 +0100

    try another test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,12 @@ DEPLOY_PATH=$2
 mkdir ~/.kube/
 mkdir ~/.aws/
 
-#printf "[profile test_deploy]\nregion=%s\n" > ~/.aws/config
-#printf "[test_deploy]\naws_access_key_id=%s\naws_secret_access_key=%s\n" $AWS_REGION $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY > ~/.aws/credentials
-
-aws configure set default.region "eu-west-1"
+aws configure --profile test_deploy <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
 
 echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 
