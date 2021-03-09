e568accd600b9b34956d635936a525b18ffcb6b3
Eivind Moland <eivind.moland@phonero.no>
2021-03-09 11:44:56 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ mkdir ~/.aws/
 #printf "[profile test_deploy]\nregion=%s\n" > ~/.aws/config
 #printf "[test_deploy]\naws_access_key_id=%s\naws_secret_access_key=%s\n" $AWS_REGION $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY > ~/.aws/credentials
 
-aws configure --profile "test_deploy"
+aws configure set default.region "eu-west-1"
 
 echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 
