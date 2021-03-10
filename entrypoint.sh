21228d0c0a28539a0ae4ade9f5ebc1640df39bd5
Eivind Moland <eivind.moland@phonero.no>
2021-03-10 11:19:22 +0100

    move validation

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,12 @@ echo $KUBE_CONFIG | base64 -d > ~/.kube/config
 # Check current version
 CURRENT_VERSION=$(kubectl get service $DEPLOYMENT_NAME -o=jsonpath='{.spec.selector.version}' --namespace=${NAMESPACE})
 
+if [[ $CURRENT_VERSION == "" ]]; then
+    echo "[DEPLOY] The deployment $DEPLOYMENT_NAME-<version> is missing, or another error occurred"
+
+    exit 1
+fi
+
 if [ "$CURRENT_VERSION" == "$VERSION" ]; then
    echo "[DEPLOY] Both versions are the same: $VERSION"
    exit 0
@@ -32,12 +38,6 @@ fi
 
 # Verify that deployment exists and get YAML definition
 DEPLOY_YAML=$(kubectl get deployment $DEPLOYMENT_NAME-$CURRENT_VERSION -o=yaml --namespace=${NAMESPACE})
-
-if [[ $DEPLOY_YAML == *"Error from server"* ]]; then
-    echo "[DEPLOY] The deployment $DEPLOYMENT_NAME-$CURRENT_VERSION is missing, or another error occurred"
-
-    exit 1
-fi
 
 # Rollout new version
 echo $DEPLOY_YAML | sed -e "s/$CURRENT_VERSION/$VERSION/g" | kubectl apply --namespace=${NAMESPACE} -f -
